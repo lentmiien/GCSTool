7575eb58f0b37609f0aa3878a79fd88664c235a6
lentmiien <lentmiien@gmail.com>
2019-09-08 20:36:22 +0900

    Fixed loading of all data from Master.json to DB

--- models/holiday.js
+++ models/holiday.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, type) => {
   return sequelize.define('holiday', {
-    name: type.DATEONLY
+    date: type.DATEONLY
   });
 };
--- models/schedule.js
+++ models/schedule.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, type) => {
   return sequelize.define('schedule', {
-    name: type.DATEONLY,
+    date: type.DATEONLY,
     work: type.BOOLEAN
   });
 };
