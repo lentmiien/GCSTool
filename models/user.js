51840e009322a70b612c7be393a81477b217204a
lentmiien <lentmiien@gmail.com>
2020-04-10 21:07:35 +0900

    Added password column

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 module.exports = (sequelize, type) => {
   return sequelize.define('user', {
     userid: type.STRING,
+    password: type.STRING,
     team: type.STRING,
-    role: type.STRING
+    role: type.STRING,
   });
 };
