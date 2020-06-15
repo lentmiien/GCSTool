1713aa4aa1884daee4da4dcfb6246a937845c647
Lennart <lentmiien@gmail.com>
2020-06-15 09:36:59 +0900

    Added input to dhl template

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -6,7 +6,7 @@ exports.page = (req, res) => {
 };
 
 exports.get_pdf = async function (req, res) {
-  fs.readFile('./data/DHL_return_request.pdf', async function (err, existingPdfBytes) {
+  fs.readFile('./data/DHL_return_request_template.pdf', async function (err, existingPdfBytes) {
     // Load a PDFDocument from the existing PDF bytes
     const pdfDoc = await PDFDocument.load(existingPdfBytes);
 
@@ -34,6 +34,60 @@ exports.get_pdf = async function (req, res) {
     firstPage.drawText(`${req.query.tracking}`, {
       x: 230,
       y: height - 211,
+      size: 12,
+      font: helveticaFont,
+      color: rgb(0, 0, 0),
+    });
+
+    // Add account number
+    firstPage.drawText(`${process.env.COMPANY_DHL_ACCOUNT}`, {
+      x: 230,
+      y: height - 241,
+      size: 12,
+      font: helveticaFont,
+      color: rgb(0, 0, 0),
+    });
+
+    // Add company name
+    firstPage.drawText(`${process.env.COMPANY_COMPANY}`, {
+      x: 230,
+      y: height - 461,
+      size: 12,
+      font: helveticaFont,
+      color: rgb(0, 0, 0),
+    });
+
+    // Add company contact person
+    firstPage.drawText(`${process.env.COMPANY_CONTACT}`, {
+      x: 230,
+      y: height - 501,
+      size: 12,
+      font: helveticaFont,
+      color: rgb(0, 0, 0),
+    });
+
+    // Add company phone number
+    firstPage.drawText(`${process.env.COMPANY_PHONE}`, {
+      x: 230,
+      y: height - 541,
+      size: 12,
+      font: helveticaFont,
+      color: rgb(0, 0, 0),
+    });
+
+    // Add company email
+    firstPage.drawText(`${process.env.COMPANY_EMAIL}`, {
+      x: 230,
+      y: height - 581,
+      size: 12,
+      font: helveticaFont,
+      color: rgb(0, 0, 0),
+    });
+
+    // Add company address
+    firstPage.drawText(`${process.env.COMPANY_ADDRESS}`, {
+      x: 230,
+      y: height - 621,
       size: 12,
       font: helveticaFont,
       color: rgb(0, 0, 0),
