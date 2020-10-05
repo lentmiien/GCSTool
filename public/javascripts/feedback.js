2f05d46730679b861e58ee1f8688d29d868b8a53
lentmiien <lentmiien@gmail.com>
2020-10-05 12:29:39 +0900

    disable btn, no empty entry

--- public/javascripts/feedback.js
+++ public/javascripts/feedback.js
@@ -1,26 +1,34 @@
 function RegisterFeedback() {
-    // Acquire datafields to send
-    const happiness = document.getElementById('happiness');
-    const type = document.getElementById('type');
-    const bug = document.getElementById('bug');
-    const comment = document.getElementById('comment');
+  // Acquire datafields to send
+  const happiness = document.getElementById('happiness');
+  const type = document.getElementById('type');
+  const bug = document.getElementById('bug');
+  const comment = document.getElementById('comment');
 
-    fetch('/meeting/addfeedback', {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-      body: JSON.stringify({
-          happiness: happiness.value,
-          type: type.value,
-          bug: bug.checked,
-          comment: comment.value,
-      }),
-    }).then((res) => {
-      happiness.value = 'neutral';
-      type.value = 'overall';
-      bug.checked = false;
-      comment.value = '';
-    });
+  if (comment.value.length == 0) {
+    alert('Please add a comment');
+    return;
   }
+
+  document.getElementById('feedbackbutton').disabled = true;
+
+  fetch('/meeting/addfeedback', {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      happiness: happiness.value,
+      type: type.value,
+      bug: bug.checked,
+      comment: comment.value,
+    }),
+  }).then((res) => {
+    happiness.value = 'neutral';
+    type.value = 'overall';
+    bug.checked = false;
+    comment.value = '';
+    document.getElementById('feedbackbutton').disabled = false;
+  });
+}
