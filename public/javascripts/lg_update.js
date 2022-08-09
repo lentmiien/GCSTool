a6c311fbc085489b444f36f114d0797a9636d8a7
Lennart <lentmiien@gmail.com>
2022-08-09 17:31:04 +0900

    update tag name

--- public/javascripts/lg_update.js
+++ public/javascripts/lg_update.js
@@ -1,10 +1,10 @@
-const updated_elements = document.querySelectorAll("[data-updatedate]");
+const updated_elements = document.querySelectorAll("[data-update]");
 const oneweek = Date.now() - (1000*60*60*24*7);
 const twoweeks = Date.now() - (1000*60*60*24*14);
 
 for (let i = 0; i < updated_elements.length; i++) {
   const e = updated_elements[i];
-  const ts = (new Date(e.dataset.updatedate)).getTime();
+  const ts = (new Date(e.dataset.update)).getTime();
   if (ts > oneweek) {
     e.style.backgroundColor = "#338833";
   } else if (ts > twoweeks) {
