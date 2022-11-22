006b5c5faabeaa6f623a61f0726bd77569f51d8c
lentmiien <lentmiien@gmail.com>
2022-11-22 10:45:35 +0900

    Update coloring

--- public/javascripts/lg_update.js
+++ public/javascripts/lg_update.js
@@ -1,13 +1,16 @@
 const updated_elements = document.querySelectorAll("[data-update]");
 const oneweek = Date.now() - (1000*60*60*24*7);
-const twoweeks = Date.now() - (1000*60*60*24*14);
+const onemonth = Date.now() - (1000*60*60*24*30);
+const threemonths = Date.now() - (1000*60*60*24*90);
 
 for (let i = 0; i < updated_elements.length; i++) {
   const e = updated_elements[i];
   const ts = (new Date(e.dataset.update)).getTime();
   if (ts > oneweek) {
     e.style.backgroundColor = "#338833";
-  } else if (ts > twoweeks) {
+  } else if (ts > onemonth) {
+    e.style.backgroundColor = "#888833";
+  } else if (ts > threemonths) {
     e.style.backgroundColor = "#333388";
   }
 }
