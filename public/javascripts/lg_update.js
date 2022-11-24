ee8af2cdbd8131e50caaaca6269d7ebb84bcda62
Lennart <lentmiien@gmail.com>
2022-11-24 11:58:40 +0900

    yellow border on within24 hrs

--- public/javascripts/lg_update.js
+++ public/javascripts/lg_update.js
@@ -1,4 +1,5 @@
 const updated_elements = document.querySelectorAll("[data-update]");
+const oneday = Date.now() - (1000*60*60*24);
 const oneweek = Date.now() - (1000*60*60*24*7);
 const onemonth = Date.now() - (1000*60*60*24*30);
 const threemonths = Date.now() - (1000*60*60*24*90);
@@ -6,6 +7,11 @@ const threemonths = Date.now() - (1000*60*60*24*90);
 for (let i = 0; i < updated_elements.length; i++) {
   const e = updated_elements[i];
   const ts = (new Date(e.dataset.update)).getTime();
+  if (ts > oneday) {
+    e.style.borderWidth = "5px";
+    e.style.borderStyle = "solid";
+    e.style.borderColor = "yellow";
+  }
   if (ts > oneweek) {
     e.style.backgroundColor = "#338833";
   } else if (ts > onemonth) {
