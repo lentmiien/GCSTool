38063158aff2a9184911936f50442ce683a8d573
lentmiien <lentmiien@gmail.com>
2023-04-30 13:28:57 +0900

    bcrypyjs

--- passport_init.js
+++ passport_init.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // Require necessary database models
 const { User } = require('./sequelize');
