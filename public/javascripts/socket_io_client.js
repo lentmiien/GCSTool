09eb7f3059e00b5e16714cec82b75294771d2699
Lennart <lentmiien@gmail.com>
2023-05-13 22:29:25 +0900

    add meeting

--- public/javascripts/socket_io_client.js
+++ public/javascripts/socket_io_client.js
@@ -52,6 +52,124 @@ document.addEventListener('DOMContentLoaded', () => {
   // Listen for the 'server response' event
   socket.on('server_response', function(data) {
     console.log(data);
+    // socket.emit('server_response', { newMeeting: [...], updateMeeting: [...], newComment: [...], updateComment: [...] });
+    const new_meeting_section = document.getElementById("new_meeting_section");
+    if (new_meeting_section) {
+      // Update as necessary
+      if (data.newMeeting.length > 0) {
+        const cnt = Date.now();
+        const sDate = data.newMeeting[0].updatedAt;
+        let classType = "alert-primary";
+        let message = "";
+        let hide = false;
+        const s_list = {"new": "新規", "prosessing": "対応中", "onhold": "待ち", "completed": "完了", "discontinued": "中止"};
+        if (data.newMeeting[0].status == "new") {
+          classType = "alert-danger";
+        }
+        if (data.newMeeting[0].status == "prosessing") {
+          classType = "alert-warning";
+        }
+        if (data.newMeeting[0].status == "completed") {
+          classType = "alert-success";
+          message = "<b>【完了】</b>";
+          hide = true;
+        }
+        if (data.newMeeting[0].status == "discontinued") {
+          classType = "alert-dark";
+          message = "<b>【中止】</b>";
+          hide = true;
+        }
+        const alert = document.createElement("div");
+        alert.classList.add("alert", classType);
+        alert.id = `meeting_${data.newMeeting[0].id}`;
+        alert.role = "alert";
+        new_meeting_section.prepend(alert);
+        const h3_title = document.createElement("h3");
+        h3_title.innerText = "■" + message + data.newMeeting[0].title;
+        alert.append(h3_title);
+        const content = document.createElement("div");
+        alert.append(content);
+        const b_status = document.createElement("b");
+        b_status.id = `meeting_status_${data.newMeeting[0].id}`;
+        b_status.innerText = `Status: ${s_list[data.newMeeting[0].status]}`;
+        content.append(b_status);
+        const i_updated = document.createElement("i");
+        i_updated.innerText = ` (Updated: ${sDate.getFullYear()}-${sDate.getMonth()+1}-${sDate.getDate()})`;
+        content.append(i_updated);
+        const pre_content = data.newMeeting[0].content;
+        pre_content.id = `meeting_content_${data.newMeeting[0].id}`;
+        content.append(pre_content);
+        const p_button = document.createElement("p");
+        content.append(p_button);
+        const button = document.createElement("button");
+        button.classList.add("btn", "btn-primary");
+        button.type = "button";
+        button.dataset.toggle = "collapse";
+        button.dataset.target = `#edit_form${cnt}`;
+        button.ariaExpanded = "false";
+        button.ariaControls = "collapseExample";
+        button.innerText = "EDIT";
+        p_button.append(button);
+        const edit_form = document.createElement("div");
+        edit_form.classList.add("collapse");
+        edit_form.id = `edit_form${cnt}`;
+        content.append(edit_form);
+        const input_group_1 = document.createElement("div");
+        input_group_1.classList.add("input-group");
+        edit_form.append(input_group_1);
+        const input_group_1_pre = document.createElement("div");
+        input_group_1_pre.classList.add("input-group-prepend");
+        input_group_1.append(input_group_1_pre);
+        const input_group_1_text = document.createElement("div");
+        input_group_1_text.classList.add("input-group-text");
+        input_group_1_text.innerText = "Status";
+        input_group_1_pre.append(input_group_1_text);
+        const input_group_1_sel = document.createElement("select");
+        input_group_1_sel.classList.add("form-control");
+        input_group_1_sel.id = `editstatus${data.newMeeting[0].id}`;
+        input_group_1_sel.name = "editstatus";
+        input_group_1.append(input_group_1_sel)
+        const input_group_1_opt1 = document.createElement("option");
+        input_group_1_opt1.value = "new";
+        input_group_1_opt1.innerText = "新規";
+        const input_group_1_opt2 = document.createElement("option");
+        input_group_1_opt2.value = "prosessing";
+        input_group_1_opt2.innerText = "対応中";
+        const input_group_1_opt3 = document.createElement("option");
+        input_group_1_opt3.value = "onhold";
+        input_group_1_opt3.innerText = "待ち";
+        const input_group_1_opt4 = document.createElement("option");
+        input_group_1_opt4.value = "completed";
+        input_group_1_opt4.innerText = "完了";
+        const input_group_1_opt5 = document.createElement("option");
+        input_group_1_opt5.value = "discontinued";
+        input_group_1_opt5.innerText = "中止";
+        input_group_1_sel.append(input_group_1_opt1, input_group_1_opt2, input_group_1_opt3, input_group_1_opt4, input_group_1_opt5);
+        const input_group_2 = document.createElement("div");
+        input_group_2.classList.add("input-group");
+        edit_form.append(input_group_2);
+        const input_group_2_pre = document.createElement("div");
+        input_group_2_pre.classList.add("input-group-prepend");
+        input_group_2.append(input_group_2_pre);
+        const input_group_2_text = document.createElement("div");
+        input_group_2_text.classList.add("input-group-text");
+        input_group_2_text.innerText = "Details";
+        input_group_2_pre.append(input_group_2_text);
+        const input_group_2_tarea = document.createElement("textarea");
+        input_group_2_tarea.classList.add("form-control");
+        input_group_2_tarea.id = `editdetails${data.newMeeting[0].id}`;
+        input_group_2_tarea.name = "editdetails";
+        input_group_2_tarea.cols = "30";
+        input_group_2_tarea.rows = "3";
+        input_group_2_tarea.value = data.newMeeting[0].content;
+        input_group_2.append(input_group_2_tarea);
+        // button
+        // hr
+        // comment section
+      }
+    } else {
+      // Alert
+    }
   });
 
   // Handle server messages
--- socket_io_controller.js
+++ socket_io_controller.js
@@ -13,6 +13,97 @@ const socketIO = require('socket.io');
 // Require necessary database models
 const { Meeting, MeetingComment } = require('./sequelize');
 
+async function createMeeting(title, created_by, content, status) {
+  /*
+    title: type.STRING,
+    created_by: type.STRING,
+    content: type.TEXT,
+    status: type.STRING,
+  */
+  try {
+    const newMeeting = await Meeting.create({ title, created_by, content, status });
+    return newMeeting;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+async function createMeetingComment(meeting_id, created_by, content) {
+  /*
+    meeting_id: type.INTEGER,
+    created_by: type.STRING,
+    content: type.TEXT,
+  */
+  try {
+    const newMeetingComment = await MeetingComment.create({ meeting_id, created_by, content });
+    return newMeetingComment;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+async function updateMeeting(id, content, status) {
+  /*
+    title: type.STRING,
+    created_by: type.STRING,
+    content: type.TEXT,
+    status: type.STRING,
+  */
+  try {
+    // Update the user with the matching id
+    const [numberOfAffectedRows, affectedRows] = await Meeting.update({
+      content,
+      status
+    }, {
+      where: { id: id },
+      returning: true, // needed for affectedRows to be populated
+      plain: true // makes sure that the returned instances are just plain objects
+    });
+
+    if(numberOfAffectedRows > 0) {
+      console.log('Update successful');
+      return affectedRows[0];
+    } else {
+      console.log('Update failed');
+      return null;
+    }
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+async function updateMeetingComment(id, content, status) {
+  /*
+    meeting_id: type.INTEGER,
+    created_by: type.STRING,
+    content: type.TEXT,
+  */
+  try {
+    // Update the user with the matching id
+    const [numberOfAffectedRows, affectedRows] = await MeetingComment.update({
+      content
+    }, {
+      where: { id: id },
+      returning: true, // needed for affectedRows to be populated
+      plain: true // makes sure that the returned instances are just plain objects
+    });
+
+    if(numberOfAffectedRows > 0) {
+      console.log('Update successful');
+      return affectedRows[0];
+    } else {
+      console.log('Update failed');
+      return null;
+    }
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
 exports.io = (server, sessionMiddleware) => {
   const io = socketIO(server);
 
@@ -34,30 +125,44 @@ exports.io = (server, sessionMiddleware) => {
     /***********
      * Meeting *
      ***********/
-    socket.on('meeting_new', (data) => {
-      console.log(data);
+    socket.on('meeting_new', async (data) => {
+      // name, title, content, comment
       // Save new meeting database entry
+      const meeting = await createMeeting(data.title, data.name, data.content, "new");
       // Save new meeting comment database entry (if given)
+      if (data.comment.length > 0) {
+        const comment = await createMeetingComment(meeting.id, data.name, data.comment);
+        meeting["comments"] = [comment];
+      } else {
+        meeting["comments"] = [];
+      }
 
       // Emit: new database entries
+      socket.emit('server_response', { newMeeting: [ meeting ], updateMeeting: [], newComment: [], updateComment: [] });
     });
-    socket.on('meeting_update', (data) => {
-      console.log(data);
+    socket.on('meeting_update', async (data) => {
+      // name, id, content, status
       // Update meeting database entry
+      const meeting = await updateMeeting(data.id, data.content, data.status);
 
       // Emit: database entry
+      socket.emit('server_response', { newMeeting: [], updateMeeting: [ meeting ], newComment: [], updateComment: [] });
     });
-    socket.on('comment_add', (data) => {
-      console.log(data);
+    socket.on('comment_add', async (data) => {
+      // name, id, comment
       // Save new meeting comment database entry
+      const comment = await createMeetingComment(data.id, data.name, data.comment);
 
       // Emit: new database entry
+      socket.emit('server_response', { newMeeting: [], updateMeeting: [], newComment: [ comment ], updateComment: [] });
     });
-    socket.on('comment_update', (data) => {
-      console.log(data);
+    socket.on('comment_update', async (data) => {
+      // id, comment
       // Update meeting comment database entry
+      const comment = await updateMeetingComment(data.id, data.comment);
 
       // Emit: database entry
+      socket.emit('server_response', { newMeeting: [], updateMeeting: [], newComment: [], updateComment: [ comment ] });
     });
 
     socket.on('disconnect', () => {
